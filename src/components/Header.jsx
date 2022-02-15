4399966661a457d372379dd1c4eed2c62308dafa
Tesla <bolarinwaahmed22@gmail.com>
2022-02-15 11:59:39 +0100

    Made the header responsive for tbalets and mobile

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,15 +3,13 @@ import "./../css/header.css";
 
 const Header = () => {
   return (
-    <React.Fragment>
-      <header>
-        <h1>AGE TODO</h1>
-        <form>
-          <input type="search" placeholder="search a todo" />
-          <button>Search</button>
-        </form>
-      </header>
-    </React.Fragment>
+    <header>
+      <h1>AGE TODO</h1>
+      <form>
+        <input type="search" placeholder="search a todo" />
+        <button>Search</button>
+      </form>
+    </header>
   );
 };
 
