40f633f65a07ea89edde511c55d7c68ed9deb59d
Tesla <bolarinwaahmed22@gmail.com>
2022-06-02 22:39:00 +0100

    Added a authentication middleware for protected routes to validate user

---
+++ backend/middlewares/authMiddleware.js
@@ -0,0 +1,28 @@
+const jwt = require("jsonwebtoken");
+const asyncHandler = require("express-async-handler");
+const User = require("./../model/userModel");
+const validateUser = asyncHandler(async (req, res, next) => {
+  let token;
+  // Checking for authorization headers
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith("Bearer")
+  ) {
+    try {
+      // Set token to the Bearer token;
+      token = req.headers.authorization.split(" ")[1];
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);
+      req.user = await User.findById(decoded.id).select("-password");
+      next();
+    } catch (error) {
+      res.status(401);
+      throw new Error("User not Authorized");
+    }
+  }
+  if (!token) {
+    res.status(401);
+    throw new Error("User not authorizd: Token not found.");
+  }
+});
+
+module.exports = validateUser;
